48c78f548fa7877ae53c5e92bff3cdee9b682e2b
Justin Martinez <justin.martinez@readytalk.com>
2015-01-02 10:56:37 -0700

    added option to block udp by using env varable for testing

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:saucy
 
 RUN apt-get -y -qq update &&\
-    apt-get install -y -q software-properties-common wget &&\
+    apt-get install -y -q software-properties-common wget iptables &&\
     add-apt-repository -y ppa:chris-lea/node.js &&\
     wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - &&\
     echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list &&\
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,18 @@
 export DISPLAY=:99
 Xvfb :99 -shmem -screen 0 1366x768x16 &
 x11vnc -display :99 -N -forever &
-# selenium must be started by a non-root user otherwise chrome can't start
-su - chromeuser -c "start-selenium"
+
+CUST_VERSION="udp"
+
+# block udp
+if [ -n "${BLOCKUDP+1}" ]; then
+  echo "udp is blocked"
+  CUST_VERSION="blockedudp"
+  iptables -A INPUT -p udp --sport 100:65535 --dport 100:65535 -j DROP
+  iptables -A OUTPUT -p udp --sport 100:65535 --dport 100:65535 -j DROP
+else
+  echo "udp is not blocked"
+fi
+
+su - chromeuser &
+start-selenium -role node -hub http://hub:4444/grid/register -browser browserName=chrome,maxInstances=20,version=${CUST_VERSION} $@
